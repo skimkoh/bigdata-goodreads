eff00370756e62a1c09b01270371caa640c1c537
kim <kohseukim@gmail.com>
2019-10-11 15:37:15 +0800

    landing page routing added

--- frontend/src/Components/Landing/Landing.js
+++ frontend/src/Components/Landing/Landing.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Table, Divider, Tag } from 'antd';
+import { Table, Button } from 'antd';
+import NavBar from '../NavBar';
 
 const data = [
     {
@@ -26,48 +27,77 @@ const data = [
     },
   ];
 
-  const columns = [
-    {
-      title: 'Book Title',
-      dataIndex: 'title',
-      key: 'title',
-      render: text => <a>{text}</a>,
-    },
-    {
-      title: 'Price ($)',
-      dataIndex: 'price',
-      key: 'price',
-    },
-    {
-      title: 'Genre',
-      dataIndex: 'genre',
-      key: 'genre',
-    },
-    {
-      title: 'Synopsis',
-      dataIndex: 'synopsis',
-      key: 'synopsis',
-    },
-    {
-      title: 'Action',
-      key: 'action',
-      render: (text, record) => (
-        <span>
-          <a>Open Book Info</a>
-        </span>
-      ),
-    },
-  ];
+  
 
 class Landing extends React.Component {
-    render(){
-        return(
-            <div>
-                <h1 style={{marginTop: 20}}>Books</h1>
-                <Table columns={columns} dataSource={data} style={{margin: 30}}/>
-            </div>
-        )
+  state = {
+    redirectBookInfo: false,
+    redirectCreateReview: false,
+  }
+
+  OpenBookInfo = () => {
+    this.setState({
+      redirectBookInfo: true,
+    })
+  }
+
+  createReview = () => {
+    this.setState({
+      redirectCreateReview: true,
+    })
+  }
+  
+
+  render(){
+    const columns = [
+      {
+        title: 'Book Title',
+        dataIndex: 'title',
+        key: 'title',
+        render: text => <a>{text}</a>,
+      },
+      {
+        title: 'Price ($)',
+        dataIndex: 'price',
+        key: 'price',
+      },
+      {
+        title: 'Genre',
+        dataIndex: 'genre',
+        key: 'genre',
+      },
+      {
+        title: 'Synopsis',
+        dataIndex: 'synopsis',
+        key: 'synopsis',
+      },
+      {
+        title: 'Action',
+        key: 'action',
+        render: (text, record) => (
+          <span>
+            <a onClick={this.OpenBookInfo}>Open Book Info</a>
+          </span>
+        ),
+      },
+    ];
+    if(this.state.redirectBookInfo){
+      this.props.history.push('/info');
     }
+
+    if(this.state.redirectCreateReview){
+      this.props.history.push('/submit');
+    }
+    
+    return(
+        <div>
+          <NavBar/>
+            <h1 style={{marginTop: 20}}>Books</h1>
+            <Button type="primary" className="createReviewbtn" onClick={this.createReview}> Create New Review </Button>
+            <Table columns={columns} dataSource={data} style={{padding: 30}}/>
+        </div>
+    )
+  }
 }
 
 
