adea861fade318b0edd5bab7cfd1e3603f152fb8
Danial Aqil <danial@Danials-MacBook-Pro.local>
2019-10-16 19:24:47 +0800

    updated forms

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,12 +6,9 @@ import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 import SubmitReviewForm from "./Components/Forms/SubmitReviewForm";
 import BookInfo from "./Components/Info/BookInfo";
 import EditReviewForm from "./Components/Forms/EditReviewForm";
-<<<<<<< HEAD
 import SubmitBookForm from "./Components/Forms/SubmitBookForm";
-=======
 import CreateBookForm from "./Components/Forms/CreateBookForm";
 import NavBar from "./Components/NavBar";
->>>>>>> 70b0aed98329ebfff8f5184d964e2f998d43574b
 
 function App() {
   return (
@@ -22,11 +19,11 @@ function App() {
           <Route path="/submit" component={SubmitReviewForm}></Route>
           <Route path="/edit" component={EditReviewForm}></Route>
           <Route path="/info" component={BookInfo}></Route>
-<<<<<<< HEAD
+
           <Route path="/booksubmit" component={SubmitBookForm}></Route>
-=======
+
           <Route path="/createbook" component={CreateBookForm}></Route>
->>>>>>> 70b0aed98329ebfff8f5184d964e2f998d43574b
+
         </div>
       </Router>
     </div>
