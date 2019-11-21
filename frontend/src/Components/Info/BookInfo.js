453aa8d502fbfbd70878fb5819853711ac7cb4d2
Chelsea Foo <chelseafooshuqi@gmail.com>
2019-11-21 14:39:34 +0800

    front end book info page changes

--- frontend/src/Components/Info/BookInfo.js
+++ frontend/src/Components/Info/BookInfo.js
@@ -1,15 +1,33 @@
 import React from "react";
 import {
-  Table, Layout, Row, Col, Modal, Checkbox, Button, List, Avatar, Icon, Rate, Divider, Tabs, Select, Dropdown, Menu, Empty, ConfigProvider, notification
+  Table,
+  Layout,
+  Row,
+  Col,
+  Modal,
+  Checkbox,
+  Button,
+  List,
+  Avatar,
+  Icon,
+  Rate,
+  Divider,
+  Tabs,
+  Select,
+  Dropdown,
+  Menu,
+  Empty,
+  ConfigProvider,
+  notification
 } from "antd";
 import NavBar from "../NavBar";
-import axios from 'axios';
-import _ from 'lodash';
+import axios from "axios";
+import _ from "lodash";
 import LoadingComponent from "../../LoadingComponent";
 
 class BookInfo extends React.Component {
   state = {
-    selectedBookID: '',
+    selectedBookID: "",
     title: null,
     asin: null,
     description: null,
@@ -20,54 +38,69 @@ class BookInfo extends React.Component {
     openNotify: false,
     loading: true,
     redirectEditReview: false,
-    reviewID: null,
+    reviewID: null
   };
 
   componentDidMount() {
-    console.log('this book has this id ' + this.props.location.state.currentBookID);
-    axios.get(`http://54.255.189.94/book/${this.props.location.state.currentBookID}`)
-    .then(res => {
-      this.setState({
-        asin: res.data['asin'],
-        title: res.data['title'],
-        price: res.data['price'],
-        description: res.data['description'],
-        selectedBookID: this.props.location.state.currentBookID,
-      })
-      return axios.get(`http://54.255.189.94/reviews/${this.props.location.state.currentBookID}`)
+    console.log(
+      "this book has this id " + this.props.location.state.currentBookID
+    );
+    axios
+      .get(
+        `http://54.255.189.94/book/${this.props.location.state.currentBookID}`
+      )
       .then(res => {
         this.setState({
-          allReviews: _.sortBy(res.data['reviews'], "overall").reverse(),
-          totalStars: Math.round((_.sumBy(res.data['reviews'], "overall") / res.data['reviews'].length) * 10) / 10,
-          loading: false,
-        })
-      })
-    })
+          asin: res.data["asin"],
+          title: res.data["title"],
+          price: res.data["price"],
+          description: res.data["description"],
+          selectedBookID: this.props.location.state.currentBookID
+        });
+        return axios
+          .get(
+            `http://54.255.189.94/reviews/${this.props.location.state.currentBookID}`
+          )
+          .then(res => {
+            this.setState({
+              allReviews: _.sortBy(res.data["reviews"], "overall").reverse(),
+              totalStars:
+                Math.round(
+                  (_.sumBy(res.data["reviews"], "overall") /
+                    res.data["reviews"].length) *
+                    10
+                ) / 10,
+              loading: false
+            });
+          });
+      });
   }
 
   sortbyTime = () => {
     this.setState({
       allReviews: _.sortBy(this.state.allReviews, "unixReviewTime").reverse()
-    })
-    console.log(this.state.allReviews)
-  }
+    });
+    console.log(this.state.allReviews);
+  };
 
   sortbyStars = () => {
     this.setState({
       allReviews: _.sortBy(this.state.allReviews, "overall")
-    })
-    console.log('SORTED BY STARS: ' + (JSON.stringify(this.state.allReviews)))
-  }
+    });
+    console.log("SORTED BY STARS: " + JSON.stringify(this.state.allReviews));
+  };
 
   sortByHelpful = () => {
     var helpfulLst = this.state.allReviews;
     helpfulLst.sort(function(a, b) {
-      return isNaN(a.helpful[1]) === isNaN(b.helpful[1]) ? a.helpful[1].localeCompare(b.helpful[1]) : (isNaN(a.helpful[1] ? -1 : 1));
+      return isNaN(a.helpful[1]) === isNaN(b.helpful[1])
+        ? a.helpful[1].localeCompare(b.helpful[1])
+        : isNaN(a.helpful[1] ? -1 : 1);
     });
     this.setState({
       allReviews: helpfulLst.reverse()
-    })
-  }
+    });
+  };
 
   createReview = () => {
     this.setState({
@@ -76,102 +109,108 @@ class BookInfo extends React.Component {
   };
 
   customizeRenderEmpty = () => (
-    <div style={{ textAlign: 'center', paddingTop: 20, paddingBottom: 20}}>
+    <div style={{ textAlign: "center", paddingTop: 20, paddingBottom: 20 }}>
       <Icon type="read" style={{ fontSize: 40, paddingBottom: 10 }} />
       <p>No reviews available. Add one!</p>
     </div>
   );
 
-  redirectEdit = (e) => {
+  redirectEdit = e => {
     this.setState({
       reviewID: e,
-      redirectEditReview: true,
-    })
-  }
+      redirectEditReview: true
+    });
+  };
 
-  showDeleteConfirm = (e) => {
-  Modal.confirm({
-    title: 'Are you sure?',
-    content: 'Do you want to proceed? This process cannot be undone.',
-    okText: 'Yes',
-    okType: 'danger',
-    cancelText: 'No',
-    onOk: () => {
-      console.log(e);
-      axios.delete(`http://54.255.189.94/review/${e}`)
-      .then((res => {
-        console.log(res);
-        notification['success']({
-          message: 'Success!',
-          description: 'Review has been deleted. Updating reviews and rating...'
+  showDeleteConfirm = e => {
+    Modal.confirm({
+      title: "Are you sure?",
+      content: "Do you want to proceed? This process cannot be undone.",
+      okText: "Yes",
+      okType: "danger",
+      cancelText: "No",
+      onOk: () => {
+        console.log(e);
+        axios.delete(`http://54.255.189.94/review/${e}`).then(res => {
+          console.log(res);
+          notification["success"]({
+            message: "Success!",
+            description:
+              "Review has been deleted. Updating reviews and rating..."
+          });
+          axios
+            .get(`http://54.255.189.94/reviews/${this.state.selectedBookID}`)
+            .then(res => {
+              this.setState({
+                allReviews: _.sortBy(res.data["reviews"], "overall").reverse(),
+                totalStars:
+                  Math.round(
+                    (_.sumBy(res.data["reviews"], "overall") /
+                      res.data["reviews"].length) *
+                      10
+                  ) / 10
+              });
+            });
         });
-        axios.get(`http://54.255.189.94/reviews/${this.state.selectedBookID}`)
-        .then(res => {
-          this.setState({
-            allReviews: _.sortBy(res.data['reviews'], "overall").reverse(),
-            totalStars: Math.round((_.sumBy(res.data['reviews'], "overall") / res.data['reviews'].length) * 10) / 10,
-          })
-        })
-      }))
-    },
-    onCancel: () => {
-    },
-  });
-  }
+      },
+      onCancel: () => {}
+    });
+  };
 
   render() {
-    if(this.state.redirectEditReview){
+    if (this.state.redirectEditReview) {
       this.props.history.push({
         pathname: "/edit",
         state: {
           selectedBookID: this.state.selectedBookID,
-          reviewID: this.state.reviewID,
-
+          reviewID: this.state.reviewID
         }
-      })
+      });
     }
 
     if (this.state.redirectCreateReview) {
       this.props.history.push({
         pathname: "/submit",
         state: {
-          selectedBookID: this.state.selectedBookID,
+          selectedBookID: this.state.selectedBookID
         }
-    })
-  }
-  let totalReviewsNum;
-  if(this.state.totalStars !== null){
-    totalReviewsNum = 
-      <div>{this.state.allReviews.length} reviews</div>
-  }
-  else{
-    totalReviewsNum = <p style={{color: '#5FB2FF'}}> No reviews available </p>
-  }
+      });
+    }
+    let totalReviewsNum;
+    if (this.state.totalStars !== null) {
+      totalReviewsNum = <div>{this.state.allReviews.length} reviews</div>;
+    } else {
+      totalReviewsNum = (
+        <p style={{ color: "#5FB2FF" }}> No reviews available </p>
+      );
+    }
 
-  let starReviews;
+    let starReviews;
 
-  if(this.state.totalStars !== null){
-    starReviews = 
-    <div>
-    <h1 style={{display: 'inline'}}>{this.state.totalStars}</h1>
-    <h4 style={{display: 'inline'}}>/5 </h4> 
-    </div>
-  }
-  else {
-    starReviews = 
-    <h4> No ratings </h4>
-  }
+    if (this.state.totalStars !== null) {
+      starReviews = (
+        <div>
+          <h1 style={{ display: "inline" }}>{this.state.totalStars}</h1>
+          <h4 style={{ display: "inline" }}>/5 </h4>
+        </div>
+      );
+    } else {
+      starReviews = <h4> No ratings </h4>;
+    }
 
-  let bookPrice;
-  if(this.state.bookPrice !== null || this.state.bookPrice !== undefined){
-    bookPrice = <h4>Price of Book: ${this.state.price}</h4>
-  }
-  else {
-    bookPrice = <h4>No Price</h4>
-  }
+    let bookPrice;
+    if (this.state.bookPrice !== null || this.state.bookPrice !== undefined) {
+      bookPrice = <h4>Price of Book: ${this.state.price}</h4>;
+    } else {
+      bookPrice = <h4>No Price</h4>;
+    }
 
-    return (
-      this.state.loading ? <div><NavBar/><LoadingComponent loading={this.state.loading}/> </div>: 
+    return this.state.loading ? (
+      <div>
+        <NavBar />
+        <LoadingComponent loading={this.state.loading} />{" "}
+      </div>
+    ) : (
       <div>
         <NavBar />
         <div className="margintop20">
@@ -180,20 +219,21 @@ class BookInfo extends React.Component {
             <Col span={4}>
               <div className="">
                 <div className="bookImgContainer">
-                <img
-                src={`http://images.amazon.com/images/P/${this.props.location.state.currentBookID}.jpg`}
-                width="150"
-                className="bookInfoPic"
-              ></img>
-              </div>
+                  <img
+                    src={`http://images.amazon.com/images/P/${this.props.location.state.currentBookID}.jpg`}
+                    width="150"
+                    className="bookInfoPic"
+                  ></img>
+                </div>
               </div>
             </Col>
             <Col span={1}></Col>
             <Col span={11}>
               <div className="floatleft marginleft20 bookInfoText">
                 <div>
-                  <h1 style={{display: 'inline'}}>{this.state.asin}</h1> 
-                  {starReviews}</div>
+                  <h1 style={{ display: "inline" }}>{this.state.asin}</h1>
+                  {starReviews}
+                </div>
                 {bookPrice}
                 {/* <h3>Genre: Science Fiction</h3> */}
                 <p>{this.state.description}</p>
@@ -208,11 +248,11 @@ class BookInfo extends React.Component {
           </Row>
         </div>
         <div className="reviewsHeader">
-        <h1 className="reviewTitle">Reviews </h1>
+          <h1 className="reviewTitle">Reviews </h1>
           <div className="reviewSub">{totalReviewsNum}</div>
-           <Button
+          <Button
             type="primary"
-            className="createReviewbtn" 
+            className="createReviewbtn"
             onClick={this.createReview}
             icon="plus"
           >
@@ -222,73 +262,93 @@ class BookInfo extends React.Component {
           {/* <Button onClick={this.sortByHelpful}>
             sort
           </Button> */}
-          <div style={{marginTop: 10}}>
-          <div className="reviewsSort"> Filters </div>
-          <Select defaultValue="stars" style={{ width: 120 }} className="floatleft reviewsSortSelect">
-            <Select.Option value="latestReview" onClick={this.sortbyTime}>
-              Latest
-            </Select.Option>
-            <Select.Option value="stars" onClick={this.sortbyStars}>
-              Most Stars
-            </Select.Option>
-             <Select.Option value="helpful" onClick={this.sortByHelpful}>
-              Helpful
-            </Select.Option>
-          </Select>
+          <div style={{ marginTop: 10 }}>
+            <div className="reviewsSort"> Filters </div>
+            <Select
+              defaultValue="stars"
+              style={{ width: 120 }}
+              className="floatleft reviewsSortSelect"
+            >
+              <Select.Option value="latestReview" onClick={this.sortbyTime}>
+                Latest
+              </Select.Option>
+              <Select.Option value="stars" onClick={this.sortbyStars}>
+                Most Stars
+              </Select.Option>
+              <Select.Option value="helpful" onClick={this.sortByHelpful}>
+                Helpful
+              </Select.Option>
+            </Select>
           </div>
-          </div>
-          
+        </div>
+
         <div className="bookReviews">
           <ConfigProvider renderEmpty={this.customizeRenderEmpty}>
             <List
-    itemLayout="vertical"
-    size="large"
-    // pagination={{
-    //   onChange: page => {
-    //     console.log(page);
-    //   },
-    //   pageSize: 10,
-    // }}
-    dataSource={this.state.allReviews}
-    renderItem={item => (
-      <List.Item
-        key={item.reviewerID}
-        className="reviewsTable"
-        actions={[
-          // <Button onClick={this.deleteReview}><Icon type="delete"></Icon>Delete</Button>
-          // <IconText type="delete" text="Delete" key="delete" onClick={this.deleteReview}/>,
-          // <IconText type="like-o" text={item.overall} key="list-vertical-like-o" />,
-          // <IconText type="message" text="2" key="list-vertical-message" />,
-        ]}
-      >
-        <List.Item.Meta
-          avatar={<Avatar size="large" icon="user" />}
-          title={
-          <div>
-            <div className="reviewSummary floatleft">{item.summary}</div>
-            <div className="reviewStar">
-              <Rate disabled value={item.overall}/>
-            </div>
-            <button className="reviewsDeleteBtn" onClick={() => this.showDeleteConfirm(item.id)}><Icon type="delete"></Icon>&nbsp; Delete</button>
-            <button className="reviewsDeleteBtn" onClick={() => this.redirectEdit(item.id)}><Icon type="edit"></Icon>&nbsp; Edit</button>
-            </div>
-            }
-          description={
-            <div style={{marginTop: 25}}>
-          <div className="floatleft">{item.reviewerName}</div>
-          <div className="reviewDate">{item.reviewTime}</div>
-          </div>  
-        }
-        />
-        <div className="reviewText">{item.reviewText}</div>
-          <div className="reviewHelpful">{item.helpful[1]} out of {item.helpful[4]} people found this review helpful.</div>
-      </List.Item>
-    )}
-  />
-  </ConfigProvider>
-         
-       
-      </div>
+              itemLayout="vertical"
+              size="large"
+              // pagination={{
+              //   onChange: page => {
+              //     console.log(page);
+              //   },
+              //   pageSize: 10,
+              // }}
+              dataSource={this.state.allReviews}
+              renderItem={item => (
+                <List.Item
+                  key={item.reviewerID}
+                  className="reviewsTable"
+                  actions={
+                    [
+                      // <Button onClick={this.deleteReview}><Icon type="delete"></Icon>Delete</Button>
+                      // <IconText type="delete" text="Delete" key="delete" onClick={this.deleteReview}/>,
+                      // <IconText type="like-o" text={item.overall} key="list-vertical-like-o" />,
+                      // <IconText type="message" text="2" key="list-vertical-message" />,
+                    ]
+                  }
+                >
+                  <List.Item.Meta
+                    avatar={<Avatar size="large" icon="user" />}
+                    title={
+                      <div>
+                        <div className="reviewSummary floatleft">
+                          {item.summary}
+                        </div>
+                        <div className="reviewStar">
+                          <Rate disabled value={item.overall} />
+                        </div>
+                        <button
+                          className="reviewsDeleteBtn"
+                          onClick={() => this.showDeleteConfirm(item.id)}
+                        >
+                          <Icon type="delete"></Icon>&nbsp; Delete
+                        </button>
+
+                        <button
+                          className="reviewsDeleteBtn"
+                          onClick={() => this.redirectEdit(item.id)}
+                        >
+                          <Icon type="edit"></Icon>&nbsp; Edit &nbsp;
+                        </button>
+                      </div>
+                    }
+                    description={
+                      <div style={{ marginTop: 25 }}>
+                        <div className="floatleft">{item.reviewerName}</div>
+                        <div className="reviewDate">{item.reviewTime}</div>
+                      </div>
+                    }
+                  />
+                  <div className="reviewText">{item.reviewText}</div>
+                  <div className="reviewHelpful">
+                    {item.helpful[1]} out of {item.helpful[4]} people found this
+                    review helpful.
+                  </div>
+                </List.Item>
+              )}
+            />
+          </ConfigProvider>
+        </div>
       </div>
     );
   }
