a47cd6f1cc0253be05c96c7bf1ad119d16ef268c
guocodes <awesomeag@hotmail.com>
2019-12-20 16:26:15 +0800

    added scripts

---
+++ ProductionScripts/launch_production_system.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo $1
+echo $2
+
+echo "setting up aws credentials"
+aws configure set aws_access_key_id $1
+aws configure set aws_secret_access_key $2
+aws configure set default.region ap-southeast-1
+aws ec2 create-key-pair --key-name zeke --query 'KeyMaterial' --output text > ../zeke.pem
+
+echo "running production_system script"
+python3 production_system_script.py -k zeke -t $3
+
